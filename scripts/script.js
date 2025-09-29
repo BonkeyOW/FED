d9540bb19ec6a37d0ca6bd43d355e1ad3bc61d9b
BonkeyOW <siltaal.nl@gmail.com>
2025-09-29 14:23:18 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,22 @@
-// JavaScript Document
-console.log("hi");
+// Hamburgermenu
+const hamburger = document.getElementById('hamburger');
+const closeMenu = document.getElementById('close-menu');
+const navMenu = document.getElementById('nav-menu');
+const submenuToggles = document.querySelectorAll('.submenu-toggle');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.add('active');
+  closeMenu.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+  navMenu.classList.remove('active');
+  closeMenu.classList.remove('active');
+});
+
+submenuToggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    const submenu = toggle.nextElementSibling;
+    submenu.classList.toggle('active');
+  });
+});
